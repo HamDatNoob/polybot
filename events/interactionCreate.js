cf77f494fc54333f5fe3282b5d4470f21e9b59f4
HamDatNoob <hamthenoob@gmail.com>
2022-03-04 21:27:16 -0500

    did stuff

---
+++ events/interactionCreate.js
@@ -0,0 +1,33 @@
+const { sleep } = require("../scripts/sleep");
+const fs = require('fs');
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction){
+        if(interaction.isCommand()){
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if(!command) return;
+
+            try{
+                await command.execute(interaction);
+            }catch(error){
+                console.error(error);
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+        }else if(interaction.isSelectMenu()){
+            const componentFiles = fs.readdirSync('./components').filter(file => file.endsWith('.js'));
+            for(const file of componentFiles){
+	            const component = require(`../components/${file}`);
+                try{
+                    await component.execute(interaction);
+                }catch(error){
+                    console.error(error);
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                }
+            }
+        }else{
+            return await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }); ;
+        }
+    }
+}; 
