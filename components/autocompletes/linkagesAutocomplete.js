a4f8607d19097774ad5b0d2a64d5d8c55c24943b
HamDatNoob <hamthenoob@gmail.com>
2022-06-15 19:30:02 -0400

    fixed term command using this autocomplete

--- components/autocompletes/linkagesAutocomplete.js
+++ components/autocompletes/linkagesAutocomplete.js
@@ -2,7 +2,7 @@ const { findBestMatch } = require("string-similarity");
 const linkages = require('../../json/linkages.json');
 
 module.exports = {
-    name: 'termAutocomplete',
+    name: 'linkageAutocomplete',
     async execute(interaction){
         const input = interaction.options._hoistedOptions[0].value.toLowerCase();
 
