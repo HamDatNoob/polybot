34782a7725d854ab312ab9fafc8d8f77b0cc01a9
ashduino101 <ashduino101@gmail.com>
2022-07-22 15:35:41 -0700

    Add main converter files

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,15 @@
+const { handleConvert } = require("../scripts/convert");
+
+
 module.exports = {
-	name: 'messageCreate',
-	async execute(message){
-		if(message.content.match(/[1-8]-\d[\dc]?c?/gmi) && !message.author.bot){
-			const interaction = undefined;
-			
-			require('../commands/level.js').execute(interaction, message);
-		}
-	}
+    name: 'messageCreate',
+    async execute(message){
+        if(message.content.match(/[1-8]-\d[\dc]?c?/gmi) && !message.author.bot){
+            const interaction = undefined;
+
+            require('../commands/level.js').execute(interaction, message);
+        }
+
+        await handleConvert(message);
+    }
 }
