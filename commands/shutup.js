f1bc38f057096fbba0c152c9f01b57252c6ecf7a
hamdatnoob <hamthenoob@gmail.com>
2023-05-14 19:11:54 -0400

    init

--- commands/shutup.js
+++ commands/shutup.js
@@ -7,7 +7,7 @@ module.exports = {
 	.setName('shutup')
 	.setDescription('Pauses public chat responses to the level command in the current channel for 15 minutes'),
 	async execute(interaction){
-		if(['715954939330953317', '461375699337609216', '767933422432682054', '461371349986836491', '767933568940638248', '880330557014294631', '461683459484549121', '875943961146032158'].some(v => interaction.member._roles.includes(v))){ // classic, masters 1 & 2, grandmaster, mod, supermod, dev, robotics
+		if(['715954939330953317', '461375699337609216', '767933422432682054', '461371349986836491', '767933568940638248', '880330557014294631', '461683459484549121', '875943961146032158', '891386997271298069'].some(v => interaction.member._roles.includes(v))){ // classic, masters 1 & 2, grandmaster, mod, supermod, dev, robotics
 			const channel = interaction.channelId;
 	
 			await db.set(`${channel}.paused`, true);
